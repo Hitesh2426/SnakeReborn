ab550d4f8a2d60ddd1cb9af65cec4e530a2c757f
Hitesh2426 <hiteshgoyal843@gmail.com>
2025-07-10 23:05:58 +0530

    Git Chnages

--- app/src/main/java/com/hiteshgoyal/snakereborn/domain/base/Constant.kt
+++ app/src/main/java/com/hiteshgoyal/snakereborn/domain/base/Constant.kt
@@ -3,5 +3,5 @@ package com.hiteshgoyal.snakereborn.domain.base
 const val DATASTORE_NAME = "snake"
 const val DATASTORE_KEY_HIGH_SCORES = "high_scores"
 const val DATASTORE_KEY_PLAYER_NAME = "player_name"
-const val REPO_URL = " https://github.com/HiteshOriginAi/SnakeReborn.git"
+const val REPO_URL = " https://github.com/Hitesh2426/SnakeReborn"
 const val TOP_10 = 10
